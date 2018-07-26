9c8083897822b4737fdbbd89729cf9f983478e49
joonij <37970610+joonij@users.noreply.github.com>
2018-07-26 17:45:08 +0900

    release 20180726

---
+++ js/javascript.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+    // 해상도 높이
+    var ht = $(window).height();
+    var conht = ht-150;
+    var fontboxht = conht/6
+    $("#content").css("height", conht);
+    $(".font_box").css("height", fontboxht);
+    $(".font_box:nth-child(1)").css("height", fontboxht*2);
+    $(".font_box:nth-child(7)").css("height", fontboxht*2);
+    $(".font_box:nth-child(12)").css("height", fontboxht*2);
+
+    // 랜덤 font
+    var fontkor = new Array("Nanum Gothic","Kirang Haerang","Nanum Myeongjo","Sunflower","Nanum Pen Script","Do Hyeon","Gaegu","Nanum Gothic Coding","Nanum Brush Script","Poor Story","Gothic A1","Black Han Sans","Jua","Hi Melody","Song Myung","Gugi","East Sea Dokdo","Gamja Flower","Stylish","Black And White ,icture","Dokdo","Cute Font","Yeon Sung");
+    var font = new Array("Roboto","Nanum Gothic","Hanalei Fill","Kirang Haerang","Open Sans","Lato","Nanum Myeongjo","Montserrat","Eczar","Roboto Condensed","Oswald","Source Sans Pro","Slabo 27px","Sunflower","Raleway","Nanum Pen Script","Merriweather","PT Sans","Roboto Slab","Do Hyeon","Poppins","Ubuntu","Noto Sans","Open Sans Condensed","Gaegu","Nanum Gothic Coding","Playfair Display","Lora","PT Serif","Titillium Web","Roboto Mono","Muli","Arimo","PT Sans Narrow","Fira Sans","Nanum ,rush Script","Inconsolata","Noto Serif","Nunito","Work Sans","Dosis","Merienda","Indie Flower","Oxygen","Josefin ,ans","Bitter","Crimson Text","Anton","Cabin","Quicksand");
+    var fontall = fontkor.concat(font);
+    var random = [];
+    var randomall = [];
+    for (var i = 0; i < fontkor.length; i++) {
+        random[i] = Math.floor(Math.random() * fontkor.length);
+        for(var j=0;j<i;j++){
+            if(random[i]==random[j]){
+                i--;
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < fontall.length; i++) {
+        randomall[i] = Math.floor(Math.random() * fontall.length);
+        for(var j=0;j<i;j++){
+            if(randomall[i]==randomall[j]){
+                i--;
+                break;
+            }
+        }
+    }
+    for(var i=0; i<17;i++) {
+        $(".font_box").eq(i).children(".infor").text('');
+        $(".font_box").eq(i).css("font-family", '');
+        $(".font_box").eq(i).children(".infor").text(fontall[randomall[i]] + " - ");
+        $(".font_box").eq(i).css("font-family", fontall[randomall[i]]);
+    }
+    $("#check_box").on("click", function(){
+        if($("#check_box").is(":checked")) {
+            for(var i=0; i<17;i++) {
+                $(".font_box").eq(i).children(".infor").text('');
+                $(".font_box").eq(i).css("font-family", '');
+                $(".font_box").eq(i).children(".infor").text(fontkor[random[i]] + " - ");
+                $(".font_box").eq(i).css("font-family", fontkor[random[i]]);
+            }
+        } else {
+            for(var i=0; i<17;i++) {
+                $(".font_box").eq(i).children(".infor").text('');
+                $(".font_box").eq(i).css("font-family", '');
+                $(".font_box").eq(i).children(".infor").text(fontall[randomall[i]] + " - ");
+                $(".font_box").eq(i).css("font-family", fontall[randomall[i]]);
+            }
+        }
+    });
+});
