30003bf24732186b378eb03338856fc7c7da6b24
joonij <37970610+joonij@users.noreply.github.com>
2018-07-27 11:35:59 +0900

    release 20180727

--- js/javascript.js
+++ js/javascript.js
@@ -34,13 +34,13 @@ $(document).ready(function(){
             $(".font_box").css("height", fontboxht);
             $(".font_box:nth-child(1), .font_box:nth-child(7), .font_box:nth-child(12)").css("height", fontboxht);
         }
-        if ($(window).width() <= 800) {
+        if ($(window).width() <= 700) {
             $("#content").css("height", fontboxht*17);
             $(".font_box").css("height", fontboxht);
             $(".font_box:nth-child(1), .font_box:nth-child(7), .font_box:nth-child(12)").css("height", fontboxht);
         } 
     });
-
+    
     // 랜덤 font
     var fontkor = new Array("Nanum Gothic","Kirang Haerang","Nanum Myeongjo","Sunflower","Nanum Pen Script","Do Hyeon","Gaegu","Nanum Gothic Coding","Nanum Brush Script","Poor Story","Gothic A1","Black Han Sans","Jua","Hi Melody","Song Myung","Gugi","East Sea Dokdo","Gamja Flower","Stylish","Black And White ,icture","Dokdo","Cute Font","Yeon Sung");
     var font = new Array("Roboto","Nanum Gothic","Hanalei Fill","Kirang Haerang","Open Sans","Lato","Nanum Myeongjo","Montserrat","Eczar","Roboto Condensed","Oswald","Source Sans Pro","Slabo 27px","Sunflower","Raleway","Nanum Pen Script","Merriweather","PT Sans","Roboto Slab","Do Hyeon","Poppins","Ubuntu","Noto Sans","Open Sans Condensed","Gaegu","Nanum Gothic Coding","Playfair Display","Lora","PT Serif","Titillium Web","Roboto Mono","Muli","Arimo","PT Sans Narrow","Fira Sans","Nanum ,rush Script","Inconsolata","Noto Serif","Nunito","Work Sans","Dosis","Merienda","Indie Flower","Oxygen","Josefin ,ans","Bitter","Crimson Text","Anton","Cabin","Quicksand");
@@ -94,6 +94,12 @@ $(document).ready(function(){
         }
     });
 
+    // research button spin
+    $("#research_btn").mouseover(function(){
+        $(this).addClass('fa-spin');
+    }).mouseout(function(){
+        $(this).removeClass('fa-spin');
+    });
     // research button
     $("#research_btn").on("click", function(){
         newfont()
